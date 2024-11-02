aa3914419683b7f4a5a3c876c985c067e438355b
Umesh Chaudhari <chaudhariumesh5534@gmail.com>
2024-11-02 23:24:19 +0530

    rebuild of repo

---
+++ config/db.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+const {MongoClient} = require("mongodb")
+require("dotenv").config();
+
+const URL = process.env.MONGO_URL;
+const DB_NAME = process.env.DB_NAME;
+let dbConnection;
+
+module.exports = {
+    connectDB: async () => {
+        try {
+            await MongoClient.connect(URL).then((client) => {
+                console.log("MongoDB Connected");
+                dbConnection = client.db(DB_NAME)
+            })
+                .catch((err) => {
+                    console.log(err);
+                })
+        } catch (error) {
+            console.error('MongoDB connection failed:', error.message);
+            process.exit(1); // Stop the server if the connection fails
+        }
+    },
+    getDb: () => {
+        return dbConnection
+    }
+}
